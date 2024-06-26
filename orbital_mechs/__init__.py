ec61c165e479a9f7fbe3e261aec1230cd06e8836
Nicolas Ehrlich <nicolas.ehrlich@yahoo.de>
2024-06-26 12:08:59 +0200

    Import just the new functions of analytic and added new analytic functions for kepler equation, flight path angle and hyperbolic excess velocity

--- orbital_mechs/__init__.py
+++ orbital_mechs/__init__.py
@@ -1,4 +1,17 @@
 from . import bodies
-from . import analytic
+
+from .analytic import (
+    circular_velocity,
+    flight_path_angle,
+    general_coplanar,
+    getTimeAtf,
+    orbital_height,
+    orbital_period,
+    orbital_position,
+    orbital_velocity,
+    plane_change
+)
+
 from . import constants
+
 from .interplanetary import Interplanetary
--- orbital_mechs/analytic.py
+++ orbital_mechs/analytic.py
@@ -1,20 +1,22 @@
-""" Analytic formulas of conic sections
+""" Analytic formulas
 """
-from math import radians, degrees, sin, cos, acos, sqrt, pi
+# Conic sections
+from math import radians, degrees, sin, cos, tan, asin, acos, atan, sqrt, pi
 
 def orbital_height(a, e, f):
     """ Orbital height for orbital position f
     Input:
-        - a [km]
-        - e [-]
-        - f [DEG]
+    :a [km]
+    :e [-]
+    :f [DEG]
     """
-    return ( a*(1-e**2) )/( 1+e*cos(radians(f)) )
+    f = radians(f)
+    return ( a*(1-e**2) )/( 1+e*cos(f) )
 
 def orbital_position(a, e, r):
     """ Orbital position for orbital height r
     """
-    return degrees(acos( (a*(1-e**2))/(e*r) - e**-1 ))
+    return degrees( acos( (a*(1-e**2))/(e*r) - e**-1 ) )
 
 def orbital_velocity(mu, a, r):
     """ Vis Viva Equation
@@ -31,14 +33,25 @@ def orbital_period(mu, a):
     """
     return 2*pi*sqrt(a**3/mu)
 
+def getTimeAtf(mu, a, e, f):
+    """ Application of the kepler equation
+    """
+    f = radians(f)
+    tanE2 = sqrt((1-e)/(1+e)) * tan(f/2)
+    E = 2*atan(tanE2)
+    if E<0:E+=2*pi
+    M = E - e*sin(E)
+    return M*sqrt(a**3/mu)
 
+# Maneuvers
 def plane_change(vi, alpha):
     """ Plane Change maneuver
     Input: 
         - Initial velocity: Vi [km/s]
         - Inclination change: alpha [deg]
         """
-    return 2*vi*sin(radians(alpha)/2)
+    alpha = radians(alpha)
+    return 2*vi*sin(alpha/2)
 
 def general_coplanar(vi, vf, alpha):
     """ General Coplanar maneuver
@@ -47,5 +60,18 @@ def general_coplanar(vi, vf, alpha):
         - Final velocity: Vf [km/s]
         - Inclination change: alpha [deg]
         """
-    dv2 = vi**2 + vf**2 - 2*vi*vf*cos(radians(alpha))
+    alpha = radians(alpha)
+    dv2 = vi**2 + vf**2 - 2*vi*vf*cos(alpha)
     return sqrt(dv2)
+
+def flight_path_angle(e, f):
+    """ Flight path angle
+    Inputs and outputs in Degree
+    """
+    f = radians(f)
+    tany = (e*sin(f))/(1+e*cos(f))
+    return degrees(atan(tany))
+
+# Hyperbolic
+def hyperbolic_excess_velocity(mu, r, C3):
+    return sqrt(2*mu/r + C3)
